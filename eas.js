4a5b4242e943354c1e2446f32a00e6a782ac0e88
Matt <mattcabpro@gmail.com>
2024-01-17 16:05:32 -0500

    streamlined UI, responsiveness, and improved js functionality

--- eas.js
+++ eas.js
@@ -1,5 +1,31 @@
+let pixelcount=16;
 grid=document.querySelector(".grid");
-for (i = 0; i < 16; i++){
-    div = document.createElement("div");
-    grid.appendChild(div);
+populateGrid();
+button=document.querySelector("button");
+
+function populateGrid (){
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+    }
+    for (i=0; i < pixelcount**2; i++){
+        percentage = 100/pixelcount + "%";
+        pixel = document.createElement("div");
+        pixel.className = "pixel";
+        pixel.style.minWidth = percentage;
+        pixel.addEventListener('mouseover', function(e){
+            if(e.buttons==1){
+                this.style.backgroundColor="red";
+            }
+        });
+        pixel.addEventListener('mousedown', function(e){ 
+            e.preventDefault();
+            this.style.backgroundColor="red";
+        });
+        grid.appendChild(pixel);
+    }
 }
+
+button.addEventListener('click', () => {
+    pixelcount = prompt("How many pixels? (Max 100)")
+    populateGrid();
+});
