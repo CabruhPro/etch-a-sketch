91c94385edf7d3247c7c1668512df122de5e9d64
CabruhPro <mattcabpro@gmail.com>
2024-01-17 18:31:16 -0500

    old branch deprecated

--- eas.js
+++ eas.js
@@ -1,10 +1,23 @@
+let pixelcount=16;
+
 grid=document.querySelector(".grid");
-for (i = 0; i < 256; i++){
-    div = document.createElement("div");
-    div.addEventListener('mouseenter', function (e) {
-        if (e.buttons == 1) {
-            this.style.backgroundColor="red";
-        }
-    });
-    grid.appendChild(div);
+button=document.querySelector("button");
+button.addEventListener("click",()=>{
+    prompt("How manyn pixels? (Max 100)")
+});
+
+function populateGrid() {
+    for (i = 0; i < pixelcount**2; i++){
+        div = document.createElement("div");
+        div.addEventListener('mouseenter', function (e) {
+            if (e.buttons == 1) {
+                percent=100/pixelcount;
+                this.style.backgroundColor="red";
+                this.style.minWidth=percent;
+            }
+        });
+        grid.appendChild(div);
+    }
 }
+populateGrid();
+
