a27cc5c1ddd257a426c110e773167f71864c3ba3
CabruhPro <mattcabpro@gmail.com>
2024-01-17 01:40:19 -0500

    basic html css js done

--- eas.js
+++ eas.js
@@ -1,5 +1,10 @@
 grid=document.querySelector(".grid");
-for (i = 0; i < 16; i++){
+for (i = 0; i < 256; i++){
     div = document.createElement("div");
+    div.addEventListener('mouseenter', function (e) {
+        if (e.buttons == 1) {
+            this.style.backgroundColor="red";
+        }
+    });
     grid.appendChild(div);
 }
